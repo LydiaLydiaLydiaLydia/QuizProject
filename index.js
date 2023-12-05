268295843a3f9fa326caef3d09b9b80128fa107b
t00222338 <lydia.sheehan2@students.ittralee.ie>
2023-12-05 12:17:32 +0000

    Must make index look better and tell the user what is going on :) x

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 //function to start the countdown
 function startTimer(){
     setTimeout(goToQuiz, 5000);
-    console.log("heya x");
+    setInterval(changeTime, 1000);
 }
 //function that happens after the countdown
 let goToQuiz = function(){
     window.open(quizLink, "_self");
 }
+let changeTime = function(){
+    countdownP.innerHTML = `${counter--}`
+}
 //getting the link to use it in my functions
 let quizLink = document.getElementById("quizLink").href;
+let countdownP = document.getElementById("timer");
+let counter = 4;
 document.onload = startTimer();
--- quizPage.js
+++ quizPage.js
@@ -1,34 +1,44 @@
 let arrayQuestions = [
     {   question: "In the ancient Humourous practice of medicine, the four Humours that control the human body are Black Bile, Phlegm, Yellow Bile, and what other?",
         possibleAnswers: ["Mucous", "Resin", "Soul", "Blood"],
-        answer: 3},
+        answer: 3,
+        hint: "This one actually exists in the human body"},
     {   question: "In what country was the Illuminati established during the Enlightenment?",
         possibleAnswers: ["England", "France", "The American Colonies", "Germany"],
-        answer: 3},
+        answer: 3,
+        hint: "The Illuminati was originally established to limit religion's influence on pubic policy"},
     {   question: "Defenestration has been a form political protest popular in Prague throughout history. What does it mean?",
         possibleAnswers: ["Throwing someone out of a window", "Debating in the town square", "Hunger strike", "To take up arms"],
-        answer: 0},
+        answer: 0,
+        hint: "Fenetre is the French word for window"},
     {   question: "In what decade of the 1900s was the American Federal Bureau of Investigation (FBI) formed?",
         possibleAnswers: ["1910s", "1950s", "1920s", "1930s"],
-       answer: 2},
+        answer: 2,
+        hint: "It was established to combat bootlegging during Prohibition"},
     {   question: "What was the first book ever printed on a printing press?",
         possibleAnswers: ["The Bible", "Shakespeare's Folio", "Plato's Dialogues", "The German Constitution"],
-        answer: 0},
+        answer: 0,
+        hint: "The Gutenberg press was invented in 1440"},
     {   question: "First produced in 1487, what was the subject of the Malleus Maleficarum?",
         possibleAnswers: ["Demonology", "Witchcraft", "Witch Hunting", "The Antichrist"],
-        answer: 2},
+        answer: 2,
+        hint: "This book was essential reading in the Salem Witch Trials"},
     {   question: "How did Cleopatra die?",
         possibleAnswers: ["Beheading", "Poisonous snake bite", "Drowning", "Fell off a chariot"],
-        answer: 1},
+        answer: 1,
+        hint: "Cleopatra died of suicide"},
     {   question: "In Ancient Greece, what was a vomitorium?",
         possibleAnswers: ["A room where you would go to vomit", "An exit", "Their word for the toilet", "An art gallery"],
-        answer: 1},
+        answer: 1,
+        hint: "To vomit means to expel"},
     {   question: "Upon exhuming their tombs, what did the British do with ancient Egyptian mummies?",
         possibleAnswers: ["Ate the preserved remains for theorised health benefits", "Ground them up and used them as fertiliser", "All of these things", "Put them in the British Museum"],
-        answer: 2},
+        answer: 2,
+        hint: "The British are strange"},
     {   question: "Where was the Statue of Liberty built?",
         possibleAnswers: ["Ohio", "France", "The Grand Canyon", "England"],
-        answer: 1}
+        answer: 1,
+        hint: "There were two statues of liberty built, and one is in Paris"}
 ];
 
 //Array to hold the randomly chosen questions
@@ -77,8 +87,9 @@ let nextQuestion = function (){
         nextButton.innerHTML = "Submit";
     }
     if(counter <= 4){
-        //loading new question and answers
-        questionH2.innerHTML = chosenQuestions[counter].question;
+        //loading new question and answers and hint
+        questionH2.innerHTML = `${chosenQuestions[counter].question}`;
+        hintP.innerHTML = chosenQuestions[counter].hint;
 
         for(let n = 0; n < answerPTags.length; n++){
             answerPTags[n].innerHTML = chosenQuestions[counter].possibleAnswers[n];
@@ -105,7 +116,7 @@ let nextQuestion = function (){
 
 //function to go to summary page
 let finishQuiz = function(){
-    if(currentAnswer == null){
+    if(currentAnswer === null){
         window.alert("Please select an answer to continue.");
         return;
     }
@@ -121,11 +132,20 @@ let finishQuiz = function(){
 
     window.open("summary.html", "", `top = ${summaryPosVert}, left = ${summaryPosHoriz}, width = ${summaryWidth}, height = ${summaryHeight}`);
 }
+
+//help event listener functions
 let getHelp = function(){
     helpBox.style.visibility = "visible";
 }
 let getHelpOff = function(){
     helpBox.style.visibility = "hidden";
+}
+//hint event listeners
+let getHint = function(){
+    hintP.style.visibility = "visible";
+}
+let getHintOff = function(){
+    hintP.style.visibility = "hidden";
 }
 
 
@@ -140,6 +160,9 @@ let questionArea = document.getElementById("possibleAnsDiv");
 //putting the question in the h2
 let questionH2 = document.getElementById("questionH2");
 questionH2.innerHTML = chosenQuestions[0].question;
+//putting the hint in the hint box
+let hintP = document.getElementById("hint");
+hintP.innerHTML = chosenQuestions[0].hint;
 //putting the multiple choice answers into paragraphs and putting them in the div
 for(let k = 0; k < chosenQuestions[0].possibleAnswers.length; k ++){
     let answerPara = document.createElement("p");
@@ -178,6 +201,9 @@ let helpMark = document.getElementsByTagName("span")[0];
 let helpBox = document.getElementById("qHelp");
 helpMark.addEventListener("mouseover", getHelp, false);
 helpMark.addEventListener("mouseout", getHelpOff, false);
+//make a mouseout and mouseover evetns for the hint for the question
+questionH2.addEventListener("mouseover", getHint, false);
+questionH2.addEventListener("mouseout", getHintOff, false);
 
 //getting the width and height of the page to make the pop-up page smaller than that
 let summaryWidth = window.innerWidth / 2;
